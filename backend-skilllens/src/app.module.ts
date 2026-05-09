49d27d4e8f18fc696913feac913c096f669fe257
Annisa-tw <nnsaisaa@gmail.com>
2026-05-09 12:05:31 +0700

    user

---
+++ backend-skilllens/src/app.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { AdminModule } from './admin/admin.module';
+import { GuruModule } from './guru/guru.module';
+import { SiswaModule } from './siswa/siswa.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'skilllens_db',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UserModule,
+    AdminModule,
+    GuruModule,
+    SiswaModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
